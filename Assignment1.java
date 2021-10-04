9bd5a9b6c61d9c88e6ed35669824399e1338f8dd
441001558 <91493979+441001558@users.noreply.github.com>
2021-10-04 22:41:56 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,1 +1,10 @@
 //your grade is 0/5
+import java.util.Scanner;
+
+//Faris hussen al alawi
+//441001558
+class Main {
+  public static void main(String[] args) {
+      //Start your code 
+      
+  }
