2b3818f064cbeb50ea016ce0f3d838f5d6273fb6
441001558 <91493979+441001558@users.noreply.github.com>
2021-12-26 19:51:42 +0300

    Create powfunction.java

---
+++ powfunction.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class  http://powFunction.java {
+    static int add(int x) {
+        return x*x;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(http://System.in);
+        int x;
+        System.out.println("Enter the number");
+        x= scan.nextInt();
+
+        System.out.println(add(x));
+
+    }
+}
