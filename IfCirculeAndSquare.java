0269fb5c1efb3d4836daef7c32dd6716f9edb311
441001558 <91493979+441001558@users.noreply.github.com>
2021-12-27 13:53:10 +0300

    Create IfCirculeAndSquare.java

---
+++ IfCirculeAndSquare.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class IfCirculeAndSquare
+{
+public static void ifCirculeAndSquare(String[] args) {
+    Scanner scan = new Scanner(http://System.in);
+System.out.println("Enter number 1 or 2");
+   int var= scan.nextInt();
+
+if (var==1){
+System.out.println("Enter semicircle");
+int var2= scan.nextInt();
+System.out.println(3.14*var2*var2);
+
+}
+else if (var==2){
+System.out.println("Enter  height");
+int var3 = scan.nextInt();
+System.out.println("Enter number offer");
+int var4 = scan.nextInt();
+System.out.println(var3 * var4);
+
+}
+
+}
+}
